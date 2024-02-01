1ac8ae87eda7c5edc4792da0a0b79e04ec9034fe
zaikodan <zaikodan@gmail.com>
2024-02-01 15:25:58 -0300

    Movement System and Visual assets
    - Implement movement logic
    - Add sprites and walking animation for different movement directions

---
+++ Blue Gravity Task/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 30f;
+
+    bool interacting;
+    IIteractable closeIteractable = null;
+    Vector2 direction;
+    Rigidbody2D rb;
+    Animator animator;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    private void Update()
+    {
+        CheckInputs();
+        Move();
+    }
+
+    private void CheckInputs()
+    {
+        direction.x = Input.GetAxis("Horizontal");
+        direction.y = Input.GetAxis("Vertical");
+
+        if (Input.GetAxis("Interact") == 1f)
+        {
+            Interact();
+        }
+    }
+
+    private void CheckDirection()
+    {
+        if (direction.x > 0)
+        {
+            transform.rotation = Quaternion.Euler(Vector2.zero);
+        }
+        else if(direction.x < 0)
+        {
+            transform.rotation = Quaternion.Euler(Vector2.up * 180);
+        }
+    }
+
+    private void Move()
+    {
+        if (!interacting)
+        {
+            rb.velocity = moveSpeed * direction;
+
+            animator.SetBool("Running", direction != Vector2.zero);
+            CheckDirection();
+        }
+    }
+
+    private void Interact()
+    {
+        if(closeIteractable != null)
+        {
+            interacting = true;
+            closeIteractable.Iteract();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        closeIteractable = collision.GetComponent<IIteractable>();
+    }
+}
